654d652f54717c1ebe6092b712d00a14e25131c3
adamaconguli <waltson2003@gmail.com>
2023-08-27 15:08:35 +0800

    move the home page to its own file

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import About from './About.jsx'
 import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<App />} />
-      <Route path='/about' element={<About />} />
-    </Routes>
-  </BrowserRouter>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
