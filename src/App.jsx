ba1fea41a36691263dd6c01c4afd668b776a30be
adamaconguli <waltson2003@gmail.com>
2023-08-28 17:44:10 +0800

    lesson 9: about page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
-import NavBar from '../components/NavBar'
+import './About.css'
 
 export default function About() {
   return (
-    <div>
-      <NavBar />
-      <h1>About</h1>
+    <div className='About'>
+      <img className='About--header' src='/images/about.png' />
+      <div className='About--content'>
+        <h3>Don’t squeeze in a sedan when you could relax in a van.</h3>
+        <p>
+          Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch. (Hitch costs extra 😉)
+          <br />
+          <br />
+          Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.
+        </p>
+        <div className='About--explore'>
+          <h4>Your destination is waiting. Your van is ready.</h4>
+          <button className='About--btn'>Explore our vans</button>
+        </div>
+      </div>
     </div>
   )
 }
